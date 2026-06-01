9f4c540ca7ebabaa4dc67206f62ac59dff8f8494
TRADICAO01 <supermercadotradicao.01@gmail.com>
2026-06-01 16:51:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const lista = [
+    "midia/oferta1.png",
+];
+
+let indice = 0;
+
+const img = document.getElementById("imagem");
+const video = document.getElementById("video");
+
+function mostrarMidia() {
+
+    const arquivo = lista[indice];
+
+    if (
+        arquivo.endsWith(".jpg") ||
+        arquivo.endsWith(".png") ||
+        arquivo.endsWith(".jpeg")
+    ) {
+
+        video.style.display = "none";
+        video.pause();
+
+        img.src = arquivo;
+        img.style.display = "block";
+
+        setTimeout(() => {
+            proximo();
+        }, 10000);
+
+    } else {
+
+        img.style.display = "none";
+
+        video.src = arquivo;
+        video.style.display = "block";
+
+        video.play();
+
+        video.onended = () => {
+            proximo();
+        };
+    }
+}
+
+function proximo() {
+
+    indice++;
+
+    if (indice >= lista.length) {
+        indice = 0;
+    }
+
+    mostrarMidia();
+}
+
+mostrarMidia();
