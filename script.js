0f55f81657888b09ce6ea13a971d1a8185f54a8e
TRADICAO01 <supermercadotradicao.01@gmail.com>
2026-06-15 19:37:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const lista = [
-    "oferta1.jpeg",
+    "0e81a825-e785-425f-b429-36b33106d6ed_D57B6FF6-02B8-410B-8B02-72DE423509CC.jpeg",
     "2b1ec8ad-6ccf-4d5f-bd66-a56a8b12f96e_06817D15-0924-4211-BBFB-9188E522017D.jpeg",
     "b2546cbf-6861-4a0c-89ec-f2d7d3f41a33_740A109F-F0F0-459C-A051-3E2CCEB83C7D.jpeg",
     "oferta4.png"
